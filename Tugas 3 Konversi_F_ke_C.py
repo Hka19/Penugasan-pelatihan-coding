4c7a012f919e95f5151c257b98a3f4cd067beb9b
Hka19 <143859108+Hka19@users.noreply.github.com>
2023-09-10 17:21:57 +0700

    Add files via upload

---
+++ Tugas 3 Konversi_F_ke_C.py
@@ -0,0 +1,12 @@
+#Latihan konversi satuan temperature
+ 
+# Program konversi Fahrenheit ke Celcius
+
+print("\nProgram Konversi Fahrenheit\n")
+
+Fahrenheit = float(input("Masukkan suhu dalam Fahrenheit = "))
+print("Suhu  Fahrenheit Adalah =",Fahrenheit, "Fahrenheit")
+
+#Celcius
+Celcius = (Fahrenheit - 32) * 5/9
+print ("Konversi dari",Fahrenheit,"F ke Celcius adalah : ",Celcius,"C" )
